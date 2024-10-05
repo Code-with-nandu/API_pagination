84ce93d15ba5097c2259c3066f1a86797f331efc
Krishnendu <krishnendudalui@gmail.com>
2024-10-05 12:30:55 +0530

    live

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+    // ///
 $route['api/users'] = 'UsersController/getPaginatedUsers';
 $route['api/users/view'] = 'UsersController/viewPaginatedUsers';
 // application/config/routes.php
--- index.php
+++ index.php
@@ -313,4 +313,4 @@ switch (ENVIRONMENT)
  * And away we go...
  */
 require_once BASEPATH.'core/CodeIgniter.php';
-require_once APPPATH . 'vendor/autoload.php';
+// require_once APPPATH . 'vendor/autoload.php';
